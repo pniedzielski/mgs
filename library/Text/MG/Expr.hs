25318fd9a7bd07fe6087a13cf5756dd8781bf81a
Patrick M. Niedzielski <patrick@pniedzielski.net>
2020-12-20 06:56:21 +0000

    Consolidate expression types

--- library/Text/MG/Expr.hs
+++ library/Text/MG/Expr.hs
@@ -3,6 +3,7 @@
 module Text.MG.Expr
   ( Chain(..)
   , Expr(..)
+  , ExprType(..)
   ) where
 
 import Text.MG.Feature
@@ -12,6 +13,9 @@ import Data.Map (Map)
 data Chain f β = Chain (FeatureStr f) β
   deriving (Eq, Ord, Show, Read, Functor)
 
+data ExprType = SimplexExpr | ComplexExpr
+  deriving (Eq, Ord, Show, Read)
+
 -- Lexical?, main chain, movers
-data Expr f β = Expr Bool (Chain f β) (Map f (Chain f β))
+data Expr f β = Expr ExprType (Chain f β) (Map f (Chain f β))
   deriving (Eq, Ord, Show, Read, Functor)
