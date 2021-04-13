2d68460707616604ad6260506b6d0f6f52fe2e17
Patrick M. Niedzielski <patrick@pniedzielski.net>
2021-04-13 05:34:50 +0000

    Add `DerivationF` functor for simple MGs

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Main (main) where
+
+import Data.List.NonEmpty( NonEmpty(..) )
 
 main ∷ IO ()
 main = do
   putStrLn "ᚣ"
+
+data LexItem f α = LexItem (NonEmpty f) α
+  deriving (Eq, Ord, Show, Functor)
+
+data DerivationF f α β
+    = Select (LexItem f α)
+    | Merge1 β β
+    | Merge2 β β
+    | Merge3 β β
+    | Move1  β
+    | Move2  β
+  deriving (Eq, Ord, Show, Functor)
