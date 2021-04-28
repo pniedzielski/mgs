e276791f91df68306726e151517b15b088ca01b5
Patrick M. Niedzielski <patrick@pniedzielski.net>
2021-04-28 20:56:30 +0000

    Add render algebra and test case

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Control.Recursion( Fix(..) )
-import Data.List.NonEmpty( NonEmpty(..) )
+import           Control.Recursion( Fix(..), cata )
+import           Data.List.NonEmpty( NonEmpty(..) )
+import qualified Data.Text    as T
+import qualified Data.Text.IO as TIO
+import           Prelude.Unicode
 
 main ∷ IO ()
 main = do
-  putStrLn "ᚣ"
+  TIO.putStrLn ∘ cata renderAlg $ deriv3
 
 data LexItem f α = LexItem (NonEmpty f) α
   deriving (Eq, Ord, Show, Functor)
@@ -21,3 +25,16 @@ data DerivationF f α β
   deriving (Eq, Ord, Show, Functor)
 
 type Derivation f α = Fix (DerivationF f α)
+
+deriv1, deriv2, deriv3 ∷ Derivation Char T.Text
+deriv1 = Fix (Select (LexItem ('a' :| ['b']) "a"))
+deriv2 = Fix (Select (LexItem ('c' :| []) "b"))
+deriv3 = Fix (Merge1 deriv1 deriv2)
+
+renderAlg ∷ DerivationF f T.Text T.Text → T.Text
+renderAlg (Select (LexItem _ s)) = s
+renderAlg (Merge1 x x') = "(Merge " <> x <> " " <> x' <> ")"
+renderAlg (Merge2 x x') = "(Merge " <> x <> " " <> x' <> ")"
+renderAlg (Merge3 x x') = "(Merge " <> x <> " " <> x' <> ")"
+renderAlg (Move1  x)    = "(Move "  <> x <> ")"
+renderAlg (Move2  x)    = "(Move "  <> x <> ")"
