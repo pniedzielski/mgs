c8f1019a6715bb95b1468c5ed4116a05c918306e
Patrick M. Niedzielski <patrick@pniedzielski.net>
2021-05-02 05:22:56 +0000

    Make features be `Text`s

--- executable/Main.hs
+++ executable/Main.hs
@@ -31,23 +31,23 @@ data Feature f
     | Licensee f
   deriving (Eq, Ord, Show, Functor)
 
-renderFeature ∷ Feature Char → T.Text
-renderFeature (Categorial f)  = T.singleton  f
-renderFeature (Selectional f) = "=" `T.snoc` f
-renderFeature (Licenser f)    = "+" `T.snoc` f
-renderFeature (Licensee f)    = "-" `T.snoc` f
+renderFeature ∷ Feature T.Text → T.Text
+renderFeature (Categorial f)  = f
+renderFeature (Selectional f) = "=" <> f
+renderFeature (Licenser f)    = "+" <> f
+renderFeature (Licensee f)    = "-" <> f
 
 data LexItem f α = LexItem (NonEmpty (Feature f)) α
   deriving (Eq, Ord, Show, Functor)
 
-renderLexItem ∷ LexItem Char T.Text → T.Text
+renderLexItem ∷ LexItem T.Text T.Text → T.Text
 renderLexItem (LexItem fs α) = "[" <> α <> " ∷ " <> fStr <> "]"
   where
     fStr = fold ∘ NonEmpty.intersperse " " $ renderFeature <$> fs
 
-li1, li2 ∷ LexItem Char T.Text
-li1 = LexItem (Selectional 'a' :| [Categorial 'b']) "a"
-li2 = LexItem (Categorial 'c' :| []) "b"
+li1, li2 ∷ LexItem T.Text T.Text
+li1 = LexItem (Selectional "a" :| [Categorial "b"]) "a"
+li2 = LexItem (Categorial "c" :| []) "b"
 
 data DerivationF f α β
     = Select (LexItem f α)
@@ -60,13 +60,13 @@ data DerivationF f α β
 
 type Derivation f α = Fix (DerivationF f α)
 
-deriv1, deriv2, deriv3, deriv4 ∷ Derivation Char T.Text
+deriv1, deriv2, deriv3, deriv4 ∷ Derivation T.Text T.Text
 deriv1 = Fix (Select li1)
 deriv2 = Fix (Select li2)
 deriv3 = Fix (Merge1 deriv1 deriv2)
 deriv4 = Fix (Merge2 deriv3 (Fix (Merge2 deriv3 deriv3)))
 
-renderDerivation ∷ Derivation Char T.Text → T.Text
+renderDerivation ∷ Derivation T.Text T.Text → T.Text
 renderDerivation = cata renderAlg
   where
     renderAlg (Select li)   = renderLexItem li
@@ -89,7 +89,7 @@ instance PP.Pretty α ⇒ PP.Pretty (Derivation f α) where
 
         sexpr = PP.parens ∘ PP.align ∘ PP.vsep
 
-treeDerivation ∷ Derivation Char T.Text → Tree.Tree String
+treeDerivation ∷ Derivation T.Text T.Text → Tree.Tree String
 treeDerivation = cata treeAlg
   where
     treeAlg (Select li) = Tree.Node
