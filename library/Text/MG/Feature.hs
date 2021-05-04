dda74ca197f740bda4c6baaf76c844d3edc256cc
Patrick M. Niedzielski <patrick@pniedzielski.net>
2021-05-04 15:42:30 +0000

    Add helper functions to `Feature` module

--- library/Text/MG/Feature.hs
+++ library/Text/MG/Feature.hs
@@ -2,11 +2,28 @@
 {-# LANGUAGE OverloadedStrings    #-}
 
 module Text.MG.Feature
-  ( Feature(..)
+  ( -- * Feature type
+    Feature(..)
+  , basicFeature
+    -- * Polarity
+  , Polarity(..)
+  , polarity
+  , isPositive
+  , isNegative
+    -- * Operation
+  , Op(..)
+  , operation
+  , isMergeable
+  , isMoveable
+    -- * Annihilation
+  , annihilates
+  , opposite
+    -- * Rendering
   , renderFeature
   ) where
 
 import Data.Text( Text )
+import Prelude.Unicode
 
 data Feature f
     = Categorial f
@@ -14,6 +31,54 @@ data Feature f
     | Licenser f
     | Licensee f
   deriving (Eq, Ord, Show, Functor)
+
+basicFeature ∷ Feature f → f
+basicFeature (Categorial f)  = f
+basicFeature (Selectional f) = f
+basicFeature (Licenser f)    = f
+basicFeature (Licensee f)    = f
+
+data Polarity = Pos | Neg
+  deriving (Eq, Ord, Show)
+
+polarity ∷ Feature f → Polarity
+polarity (Categorial _)  = Neg
+polarity (Selectional _) = Pos
+polarity (Licenser _)    = Pos
+polarity (Licensee _)    = Neg
+
+isPositive ∷ Feature f → Bool
+isPositive = (≡ Pos) ∘ polarity
+
+isNegative ∷ Feature f → Bool
+isNegative = (≡ Neg) ∘ polarity
+
+data Op = OpMerge | OpMove
+  deriving (Eq, Ord, Show)
+
+operation ∷ Feature f → Op
+operation (Categorial _)  = OpMerge
+operation (Selectional _) = OpMerge
+operation (Licenser _)    = OpMove
+operation (Licensee _)    = OpMove
+
+isMergeable ∷ Feature f → Bool
+isMergeable = (≡ OpMerge) ∘ operation
+
+isMoveable ∷ Feature f → Bool
+isMoveable = (≡ OpMove) ∘ operation
+
+annihilates ∷ Eq f ⇒ Feature f → Feature f → Bool
+annihilates f f'
+    = polarity f     ≢ polarity f'
+    ∧ operation f    ≡ operation f'
+    ∧ basicFeature f ≡ basicFeature f'
+
+opposite ∷ Feature f → Feature f
+opposite (Categorial f)  = Selectional f
+opposite (Selectional f) = Categorial f
+opposite (Licenser f)    = Licensee f
+opposite (Licensee f)    = Licenser f
 
 renderFeature ∷ Feature Text → Text
 renderFeature (Categorial f)  = f
