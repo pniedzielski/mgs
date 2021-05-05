2dea18021ddfc31e4be16474c5b159af24186f1a
Patrick M. Niedzielski <patrick@pniedzielski.net>
2021-05-05 05:15:04 +0000

    Add parser for `LexItem`s

--- executable/Main.hs
+++ executable/Main.hs
@@ -6,6 +6,7 @@ module Main (main) where
 
 import           Control.Recursion( Fix(..) )
 import           Data.List.NonEmpty( NonEmpty(..) )
+import           Data.Maybe( fromJust )
 import qualified Data.Text                 as T
 import qualified Data.Text.IO              as TIO
 import qualified Data.Text.Prettyprint.Doc as PP
@@ -21,21 +22,26 @@ main ∷ IO ()
 main = do
   parseTest (parseFeatureList <* eof) "=d +k =d  v -foc"
   parseTest (parseFeatureList <* eof) "d =n -k"
+  parseTest (parseLexItem <* eof) "what∷ D -wh"
+  parseTest (parseLexItem <* eof) "that ::    =T C"
   TIO.putStrLn ∘ renderLexItem $ li1
   TIO.putStrLn ∘ renderLexItem $ li2
-  TIO.putStrLn ∘ renderDerivation $ deriv3
-  putDoc ∘ PP.pretty $ deriv4
+  TIO.putStrLn ∘ renderLexItem $ li3
+  TIO.putStrLn ∘ renderDerivation $ deriv4
+  putDoc ∘ PP.pretty $ deriv5
   TIO.putStrLn ""
-  putStrLn ∘ Tree.drawTree ∘ treeDerivation $ deriv4
+  putStrLn ∘ Tree.drawTree ∘ treeDerivation $ deriv5
 
-li1, li2 ∷ LexItem T.Text T.Text
-li1 = LexItem (Selectional "a" :| [Categorial "b"]) "a"
-li2 = LexItem (Categorial "c" :| []) "b"
+li1, li2, li3 ∷ LexItem T.Text T.Text
+li1 = fromJust $ parseMaybe parseLexItem "Brutus ∷ d"
+li2 = fromJust $ parseMaybe parseLexItem "Caesar ∷ d"
+li3 = fromJust $ parseMaybe parseLexItem "stabbed ∷ =d =d v"
 
-deriv1, deriv2, deriv3, deriv4 ∷ Derivation T.Text T.Text
+deriv1, deriv2, deriv3, deriv4, deriv5 ∷ Derivation T.Text T.Text
 deriv1 = Fix (Select li1)
 deriv2 = Fix (Select li2)
-deriv3 = Fix (Merge1 deriv1 deriv2)
-deriv4 = Fix (Merge2 deriv3 (Fix (Merge2 deriv3 deriv3)))
+deriv3 = Fix (Select li3)
+deriv4 = Fix (Merge1 deriv3 deriv2)
+deriv5 = Fix (Merge2 deriv4 deriv1)
 
 
--- library/Text/MG/Grammar.hs
+++ library/Text/MG/Grammar.hs
@@ -4,12 +4,17 @@
 module Text.MG.Grammar
     ( LexItem(..)
     , renderLexItem
+    , parseLexItem
     ) where
 
+import           Control.Monad( void )
 import           Data.Foldable( fold )
 import           Data.List.NonEmpty( NonEmpty(..) )
 import qualified Data.List.NonEmpty        as NonEmpty
 import qualified Data.Text                 as T
+import           Data.Void
+import           Text.Megaparsec hiding( State )
+import           Text.Megaparsec.Char( letterChar, hspace, string )
 import           Text.MG.Feature
 import           Prelude.Unicode
 
@@ -17,6 +22,16 @@ data LexItem f α = LexItem (NonEmpty (Feature f)) α
   deriving (Eq, Ord, Show, Functor)
 
 renderLexItem ∷ LexItem T.Text T.Text → T.Text
-renderLexItem (LexItem fs α) = "[" <> α <> " ∷ " <> fStr <> "]"
+renderLexItem (LexItem fs α) = α <> " ∷ " <> fStr
   where
     fStr = fold ∘ NonEmpty.intersperse " " $ renderFeature <$> fs
+
+parseLexItem ∷ Parsec Void T.Text (LexItem T.Text T.Text)
+parseLexItem =
+  do
+    identifier ← T.pack <$> many letterChar
+    void separator
+    features ← parseFeatureList
+    return $ LexItem features identifier
+  where
+    separator = hspace *> (string "∷" <|> string "::") <* hspace
