a9cacd5a2a0f6e39f017575a297ddabe3f983328
Patrick Michael Niedzielski <patrick@pniedzielski.net>
2021-12-29 21:06:25 +0000

    Make application parse

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,12 +1,21 @@
 module Main (main) where
 
 import Data.Maybe ( fromJust )
+import qualified Data.Tree
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Text.Megaparsec ( parseMaybe )
+import Text.MG
+import Text.MG.Algebra.DerivationTree
 import Text.MG.IO ( formatGrammar, parseMG )
+import Text.MG.Parser.BottomUp
+import Prelude.Unicode
 import System.Environment ( getArgs )
 
 -------------------------------------------------------------------------------
+
+drawTree ∷ Data.Tree.Tree T.Text → T.Text
+drawTree = T.pack ∘ Data.Tree.drawTree ∘ fmap T.unpack
 
 main ∷ IO ()
 main = do
@@ -14,3 +23,10 @@ main = do
   grammarText ← TIO.readFile $ head args
   let grammar = fromJust $ parseMaybe parseMG grammarText
   TIO.putStr $ formatGrammar grammar
+
+  str ← TIO.getLine
+  let (chart', df') = fillChart grammar $ T.words str
+  TIO.putStr $ showChart chart'
+  let n = length ∘ T.words $ str
+  let c = startCategory grammar
+  TIO.putStr ∘ drawTree ∘ derivationTree ∘ head $ concatMap (derivations df') (doneItems chart' c n)
