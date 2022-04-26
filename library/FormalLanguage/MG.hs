aa2934215be0cfdc993da2a498fcc3e5b341e145
Patrick M. Niedzielski <patrick@pniedzielski.net>
2022-04-26 20:11:28 +0000

    Add initial MG module
    At the moment, we have no MG specific code in our package.  While we
    will eventually add modules for specific MG-related functionality, it
    is useful to have an overarching module that re-exports the most
    common MG functionality, to avoid users needing to import each small
    module they need to use.  This patch adds such a module, which is at
    the moment devoid of exports.
    
    The module this patch adds is named `FormalLanguage.MG`.  This name
    mirrors that of another package that already exists, for working with
    CFGs, which lives under the `FormalLanguage` namespace.  At the
    moment, the module consists of documentation describing what a
    Minimalist Grammar is, and what the purpose of this module is, along
    with a short bibliography of references.
    
    In order for the module to be compiled, we need to add a library to
    our Hpack configuration, pointing to subdirectory where the library is
    contained.  Because the module doesn’t export anything at the moment,
    this patch does not yet add imports to the executable or tests for the
    module, which would not have any use.
    
    Testing
    -------
    
    This patch does not introduce any functionality, but it does change
    the build system.  After applying the patch, it was verified that the
    package still builds without warnings by running:
    
        stack build --test --haddock
    
    License: BSD-3-Clause
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>
    Tested-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

---
+++ library/FormalLanguage/MG.hs
@@ -0,0 +1,38 @@
+{-# OPTIONS_HADDOCK not-home #-}
+-------------------------------------------------------------------------------
+-- |
+-- Module      : FormalLanguage.MG
+-- Description : Common tools for working with Minimalist Grammars
+-- Copyright   : © 2022, Patrick M. Niedzielski
+-- License     : BSD-3-Clause
+--
+-- Maintainer  : patrick@pniedzielski.net
+-- Stability   : alpha
+-- Portability : portable
+--
+-- Minimalist Grammars (“MGs”; Stabler 1997, Stabler & Keenan 2003, etc) are a
+-- class of closely related grammatical formalisms that incorporate ideas from
+-- Chomsky’s Minimalist Program of linguistic theory, such as covert and overt
+-- phrasal movement, head movement, remnant movement, and others, while still
+-- being simple enough to prove important computational properties about
+-- theories stated in these terms.
+--
+-- This module exports the most common tools for working with Minimalist
+-- Grammars.  Additional functionality, such as parsers, grammatical morphisms,
+-- and Minimalist Grammar algebras can be imported from other modules included
+-- in this package.
+
+module FormalLanguage.MG
+  ( -- * Bibliography
+    --
+    -- $bibliography
+  ) where
+
+-- $bibliography
+--
+--   * Stabler, E. (1997).  Derivational minimalism.  In Proceedings of the
+--     First International Conference on Logical Aspects of Computational
+--     Linguistics (LACL 1996), pp. 68–95.
+--
+--   * Stabler, E. and E. Keenan (2003).  Structural similarity within and
+--     among languages.  Theoretical Computer Science 293(2), pp. 345–363.
